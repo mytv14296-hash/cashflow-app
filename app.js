6d0fe6e8b0113b4760b59b32debb8e59fb0b25a9
mytv14296-hash <mytv14296@gmail.com>
2026-06-27 18:47:10 +0700

    app.js

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const API_URL = "https://script.google.com/macros/s/AKfycbwHcpDVvc0WSgq7P4KlMlJ-9VhRfK-7tD5dR25jiKZlc9QnxdOnM6-mVTGzsvY-7RKDvg/exec";
-
 let saldo = 0;
 let totalMasuk = 0;
 let totalKeluar = 0;
